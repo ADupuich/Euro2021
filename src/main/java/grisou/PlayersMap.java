bbb5bc223824eaad129001c267dabbf8aa87a18a
Amaury Dupuich <amaury.dupuich@live.fr>
2021-06-22 16:04:26 +0200

    Commit suite au RdV mentor

--- src/main/java/grisou/PlayersMap.java
+++ src/main/java/grisou/PlayersMap.java
@@ -5,17 +5,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class PlayersMap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		final ArrayList<String> joueurs = new ArrayList<String>();
 
 		try {
 			final BufferedReader reader = new BufferedReader(
-					new FileReader("/home/amaury/eclipse-workspace/grisou/src/main/java/Joueurs.txt"));
+					new FileReader("C:\\Users\\Amaur\\Documents\\Euro2021\\src\\main\\java\\Joueurs.txt"));
 			String line = reader.readLine();
 
 			while (line != null) {
@@ -27,24 +26,21 @@ public class PlayersMap {
 			e.printStackTrace();
 		}
 
+		final FileWriter writer = new FileWriter("howManyLetters.txt");
+		Integer nombreDeLettres;
+		final String Newligne = System.getProperty("line.separator");
+
 		for (String str : joueurs) {
 
 			try {
+				nombreDeLettres = str.length();
+				writer.write(str + " : " + nombreDeLettres + Newligne);
 
-				final FileWriter writer = new FileWriter("howManyLetters.txt");
-				Integer nombreDeLettres;
-				final Iterator<String> iterator = joueurs.iterator();
-				final String Newligne = System.getProperty("line.separator");
-				while (iterator.hasNext()) {
-					str = iterator.next();
-					nombreDeLettres = str.length();
-					writer.write(str + " : " + nombreDeLettres + Newligne);
-				}
-				writer.close();
 			} catch (final IOException e) {
 				e.printStackTrace();
 			}
 		}
+		writer.close();
 	}
 
 }
