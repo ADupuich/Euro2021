32995b0ab856052d8959582a39e4d0cdcef33a9f
Amaury Dupuich <amaury.dupuich@live.fr>
2021-06-23 14:44:45 +0200

    c'est la zone

--- src/main/java/fr/adupuich/grisou/EnregistreLeFichier.java
+++ src/main/java/fr/adupuich/grisou/EnregistreLeFichier.java
@@ -6,28 +6,29 @@ import java.util.ArrayList;
 
 public class EnregistreLeFichier implements IFichier {
 
-	public EnregistreLeFichier(ArrayList<String> joueurs) throws IOException {
+	@Override
+	public void EnregistreLeFichier(ArrayList<String> joueurs) {
 
-		final FileWriter writer = new FileWriter("howManyLetters.txt");
 		Integer nombreDeLettres;
 		final String Newligne = System.getProperty("line.separator");
 
 		for (final String str : joueurs) {
 
+			final FileWriter writer = new FileWriter("howManyLetters.txt");
+
 			try {
 				nombreDeLettres = str.length();
 				writer.write(str + " : " + nombreDeLettres + Newligne);
+				while (iterator.hasNext()) {
+					joueurs = iterator.next();
+				}
 
+				writer.close();
 			} catch (final IOException e) {
 				e.printStackTrace();
 			}
-		}
-		writer.close();
-	}
 
-	@Override
-	public void EnregistreLeFichier(ArrayList<String> joueurs) {
-		// TODO Auto-generated method stub
+		}
 
 	}
 
